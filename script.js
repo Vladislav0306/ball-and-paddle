cedda677768bd10dd92007751c3fb47ed9f8efe1
Vladislav0306 <ezkovvladislav@gmail.com>
2023-12-29 09:55:21 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,93 @@
+var canvas = null;
+var ctx = null;
+var mouseX = null;
+
+var frames = 24;
+
+var ballX = 50;
+var ballY = 50;
+var ballStepX = 5;
+var ballStepY = 6;
+var ballRadius = 10;
+
+var paddleX = null;
+var paddleWidth = 100;
+var paddleHeight = 10;
+var paddleOffset = 40;
+
+function mouseCoords (event) {
+  var canvasOffset = canvas.getBoundingClientRect();
+  var htmlElement = document.documentElement;
+  mouseX = event.clientX - canvasOffset.left - htmlElement.scrollLeft;
+  paddleX = mouseX - paddleWidth/2;
+}
+
+function drawRect (leftX, leftY, boxWidth, boxHeight, boxFillColor) {
+  ctx.fillStyle = boxFillColor;
+  ctx.fillRect(leftX, leftY, boxWidth, boxHeight);
+}
+
+function drawBall(centerX, centerY, radius, fillColor) {
+  ctx.fillStyle = fillColor;
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, radius, 0, 360*Math.PI/180, true);
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawAll() {
+  drawRect(0, 0, canvas.width, canvas.height, '#000');
+  drawBall(ballX, ballY, ballRadius, 'firebrick');
+  drawRect(paddleX, canvas.height - paddleOffset, paddleWidth, paddleHeight, '#fff');
+}
+
+function moveAll() {
+
+  var paddleLeftEdge = paddleX;
+  var paddleRightEdge = paddleLeftEdge + paddleWidth;
+  var paddleTopEdge = canvas.height - paddleOffset;
+  var paddleBottomEdge = paddleTopEdge + paddleHeight;
+
+  ballX += ballStepX;
+  ballY += ballStepY;
+
+  if ( ballX < 0 || ballX > canvas.width) {
+    ballStepX *= -1;
+  }
+  if ( ballY < 0 ) {
+    ballStepY *= -1;
+  }
+  if ( ballX > paddleLeftEdge && ballX < paddleRightEdge && ballY > paddleTopEdge && ballY < paddleBottomEdge ) {
+    ballStepY *= -1;
+    var paddleCenter = paddleLeftEdge + paddleWidth/2;
+    var ballDistance = ballX - paddleCenter;
+    ballStepX = ballDistance * 0.35;
+  }
+  if ( ballY > canvas.height ) {
+    drawBall();
+    alert("Game Over");
+ }
+}
+
+function updateAll() {
+  setInterval( function () {
+    moveAll();
+    drawAll();
+  }, 1000/frames);
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+
+  canvas = document.querySelector('#canvas');
+  ctx = canvas.getContext('2d');
+
+  if ( ctx ) {
+
+    canvas.width = 800;
+    canvas.height = 400;
+
+    updateAll();
+    canvas.addEventListener('mousemove', mouseCoords, false);
+
+  }
+}, false);
